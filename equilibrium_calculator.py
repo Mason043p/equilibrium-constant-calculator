f9f3216790bec70af2b37ea420b9d0e88953244e
Mason043p <74705524+Mason043p@users.noreply.github.com>
2021-01-14 21:19:47 -0500

    Update equilibrium_calculator.py

--- equilibrium_calculator.py
+++ equilibrium_calculator.py
@@ -1,9 +1,12 @@
-a = float(input("What is a's starting value?"))
-b = float(input("What is b's starting value? "))
-
-forward_rate_constant = float(input("What is the value of the forward rate constant as a decimal? "))
-reverse_rate_constant = float(input("What is the value of the reverse rate constant as a decimal? "))
-number_of_calculations = int(input("How many calculations would you like to make? "))
+while true:
+try:
+    a = float(input("What is a's starting value?"))
+    b = float(input("What is b's starting value? "))
+    forward_rate_constant = float(input("What is the value of the forward rate constant as a decimal? "))
+    reverse_rate_constant = float(input("What is the value of the reverse rate constant as a decimal? "))
+    number_of_calculations = int(input("How many calculations would you like to make? "))
+except ValueError:
+    print("Please input a number, not letters")     
 
 def formula():
     
